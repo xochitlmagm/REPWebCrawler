1dddd82acfc4b7a98bba5320ff49f3903cdf1c4a
Aly Davies <alydavies@Aly.T-mobile.com>
2020-12-14 14:31:26 -0800

    Data Input First Commit

---
+++ ProcessInput.py
@@ -0,0 +1,51 @@
+import pandas
+
+
+class HouseNode:
+    def __init__(self, address, city, state, price, num_beds, num_baths, sq_footage, lot, backyard, garage, basement):
+        self.address = address
+        self.city = city
+        self.state = state
+        self.price = price
+        self.num_beds = num_beds
+        self.num_baths = num_baths
+        self.sq_footage = sq_footage
+        self.lot = lot
+        self.backyard = backyard
+        self.garage = garage
+        self.basement = basement
+        self.score = 0
+
+    def home_value_score(self):
+        # TODO Replace with value function, then update self.score with it.
+        self.assertEqual(True, False)
+
+
+def read_from_file_pandas():
+    houses_read = pandas.read_csv('houses.csv')
+    houses_read = houses_read.sort_values('Price')
+    houses_processing_list = houses_read.values.tolist()
+    houses_list = []
+    # print(house_address)
+    for h in houses_processing_list:
+        address = h[0]
+        city = h[1]
+        state = h[2]
+        price = h[3]
+        num_beds = h[4]
+        num_baths = h[5]
+        sq_footage = h[6]
+        year_built = h[8]
+        garage = h[9]
+        backyard = h[10]
+        basement = h[11]
+        houses_list.append(HouseNode(address, city, state, price, num_beds, num_baths, sq_footage, year_built, garage, backyard, basement))
+
+    return houses_list
+
+if __name__ == '__main__':
+    houses = read_from_file_pandas()
+    for l in houses:
+        print(l.address)
+
+
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Main requires pandas library that is available for anaconda.
+See installation and documentation here: https://pandas.pydata.org/docs/getting_started/index.html
